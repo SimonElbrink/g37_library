657aa7acde18230e60e6bcd25914fa8a627fa02b
Simon Elbrink <simon.elbrink@gmail.com>
2021-09-14 10:20:21 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/workshop/g37_library/G37LibraryApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop.g37_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G37LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G37LibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/workshop/g37_library/G37LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop.g37_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G37LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
