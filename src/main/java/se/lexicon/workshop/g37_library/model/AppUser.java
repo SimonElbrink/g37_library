6fb990b3ce7a33fc6ad54335c503a0c33e0c08ac
Simon Elbrink <simon.elbrink@gmail.com>
2021-09-16 10:49:19 +0200

    Part 5  point 1,2,3
    1. Add a List of entity “BookLoan” in AppUser
    2. Set up OneToMany relationship in AppUser
    3.Add method in AppUser to add BookLoan in a bidirectional way.
    
    OrphanRemoval -True Why? If the Loan does not have a AppUser what is the point to keep it?
    
    Custom Setter. Why? For updating the relationship in a bidirectional way.
    
    AddBookLoan and RemoveBookLoan. Why? To handle the Relations in bidirectional way.

--- src/main/java/se/lexicon/workshop/g37_library/model/AppUser.java
+++ src/main/java/se/lexicon/workshop/g37_library/model/AppUser.java
@@ -2,6 +2,8 @@ package se.lexicon.workshop.g37_library.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -18,23 +20,85 @@ public class AppUser {
             fetch = FetchType.EAGER)
     private Details userDetails;
 
+    @OneToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH},
+            fetch = FetchType.LAZY,
+            orphanRemoval = true)
+    private List<BookLoan> loans;
+
     public AppUser() {
     }
 
-    public AppUser(int appUserId, String username, String password, LocalDate regDate, Details userDetails) {
-        this.appUserId = appUserId;
-        this.username = username;
-        this.password = password;
-        this.regDate = regDate;
-        this.userDetails = userDetails;
+    public AppUser(int appUserId, String username, String password, LocalDate regDate, Details userDetails, List<BookLoan> loans) {
+
+        setAppUserId(appUserId);
+        setUsername(username);
+        setPassword(password);
+        setRegDate(regDate);
+        setUserDetails(userDetails);
+        setLoans(loans);
     }
 
     public AppUser(String username, String password, Details userDetails) {
-        this.username = username;
-        this.password = password;
-        this.userDetails = userDetails;
+        this(0, username, password, LocalDate.now(), userDetails, new ArrayList<>());
 
-        this.regDate = LocalDate.now();
+    }
+
+    public boolean addBookLoan(BookLoan bookLoan) {
+        if (bookLoan == null) throw new NullPointerException("bookLoan was null");
+        if (loans == null) loans = new ArrayList<>();
+
+        if (!loans.contains(bookLoan)) {
+            bookLoan.setBorrower(this);
+            this.loans.add(bookLoan);
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public boolean removeBookLoan(BookLoan bookLoan) {
+        if (bookLoan == null) throw new NullPointerException("bookLoan was null");
+        if (loans == null) loans = new ArrayList<>();
+
+        if (this.loans.remove(bookLoan)) {
+            bookLoan.setBorrower(null);
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+    public void setLoans(List<BookLoan> loans) {
+        if (loans == null) loans = new ArrayList<>();
+        if (this.loans == null) this.loans = new ArrayList<>();
+
+
+        while (loans.contains(null)) {
+            loans.remove(null);
+        }
+
+
+        if (loans.isEmpty()) {
+            if (this.loans != null) {
+                for (BookLoan loan : this.loans) {
+                    loan.setBorrower(null);
+                }
+            }
+        } else {
+            for (BookLoan loan : loans) {
+                loan.setBorrower(this);
+            }
+        }
+
+        this.loans = loans;
+    }
+
+    public List<BookLoan> getLoans() {
+        if (loans == null) loans = new ArrayList<>();
+        return loans;
     }
 
     public int getAppUserId() {
